01ba16cc76bbbc882a868fb80a38e69f2037e263
DJ OMiY <onemoreinsanity@gmail.com>
2013-06-14 00:27:43 -0400

    made changes to combo font
    changed texture font file and code in Player Combo lua file

--- Graphics/Player combo/default.lua
+++ Graphics/Player combo/default.lua
@@ -63,7 +63,7 @@ local t = Def.ActorFrame {
 				or params.TapNoteScore == 'TapNoteScore_W4' 
 				or params.TapNoteScore == 'TapNoteScore_W5' 
 				or params.TapNoteScore == 'TapNoteScore_Miss' then
-				lsatWorstJudge[p] = 1;
+				lsatWorstJudge[p] = 48;
 			end;
 		end;
 	end;
@@ -88,13 +88,13 @@ local t = Def.ActorFrame {
 		cf.Number:settext( string.format("%i", iCombo) );
 
 		if param.Combo and lsatWorstJudge[p] == 1 then
-			(cmd(stopeffect;diffuseshift;effectperiod,1;effectcolor1,color("1,1,1,1");effectcolor2,color("0.75,0.75,0.75,1");strokecolor,color("0.5,0.5,0.5,1")))(cf.Number);
+			(cmd(stopeffect;diffuseshift;effectperiod,1;effectcolor1,color("0.7,0.7,1,1");effectcolor2,color("1,1,1,1");strokecolor,color("0.5,0.5,0.5,1")))(cf.Number);
 		elseif param.Combo and lsatWorstJudge[p] == 2 then
 			(cmd(stopeffect;diffuse,color("1,0.9,0.3,1");strokecolor,color("0.2,0.1,0,1")))(cf.Number);
 		elseif param.Combo and lsatWorstJudge[p] == 3 then
 			(cmd(stopeffect;diffuse,color("0.5,1,0.4,1");strokecolor,color("0,0.2,0,1")))(cf.Number);
 		elseif param.Combo then
-			cf.Number:stopeffect();
+			(cmd(stopeffect;diffuse,color("1,1,1,1");strokecolor,color("0.5,0.5,0.5,0")))(cf.Number);
 		else
 			(cmd(stopeffect;diffuse,color("1,0,0,1");strokecolor,color("0.5,0,0,1")))(cf.Number);
 			(cmd(diffuse,Color("Red");diffusebottomedge,color("0.5,0,0,1")))(cf.Label);
